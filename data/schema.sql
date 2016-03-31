76041e7a2e9c4e30307dbdf43f41070ef33626a2
Shaunak Kashyap <ycombinator@gmail.com>
2016-03-31 10:31:16 -0400

    Schema updates.

--- data/schema.sql
+++ data/schema.sql
@@ -4,10 +4,12 @@ USE blog;
 
 CREATE TABLE IF NOT EXISTS authors (
     id            INTEGER        NOT NULL    AUTO_INCREMENT,
+    email         VARCHAR(255)   NOT NULL,
     first_name    VARCHAR(255)   NOT NULL,
     last_name     VARCHAR(255)   NOT NULL,
     organization  VARCHAR(255),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE KEY (email)
 );
 
 CREATE TABLE IF NOT EXISTS posts (
@@ -18,5 +20,20 @@ CREATE TABLE IF NOT EXISTS posts (
     body          TEXT           NOT NULL,
     publish_date  DATETIME       NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (author_id) REFERENCES authors(id)
+    FOREIGN KEY (author_id) REFERENCES authors(id),
+    FULLTEXT (title, abstract, body)
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+    id            INTEGER       NOT NULL    AUTO_INCREMENT,
+    tag           VARCHAR(255)  NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (tag)
+);
+
+CREATE TABLE IF NOT EXISTS posts_tags (
+    post_id       INTEGER       NOT NULL,
+    tag_id        INTEGER       NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+    FOREIGN KEY (tag_id) REFERENCES tags(id)
 );
