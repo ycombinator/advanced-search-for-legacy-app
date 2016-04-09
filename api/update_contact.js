14336a3a7195cad4c4019bc264f2fcdaee879256
Shaunak Kashyap <ycombinator@gmail.com>
2016-04-09 15:07:19 -0500

    Augmenting update code to write to Elasticsearch

--- api/update_contact.js
+++ api/update_contact.js
@@ -1,5 +1,6 @@
 const async = require('async');
 const connection = require('./mysql_connection');
+const elasticsearch = require('./elasticsearch_connection');
 
 module.exports = function(request, reply) {
 
@@ -25,7 +26,7 @@ module.exports = function(request, reply) {
       ';
     connection.query(query, [
       request.payload.line_1.toLowerCase(),
-      (typeof request.payload.line_2 !== 'undefined' ? request.payload.line_2.toLowerCase() : ''),
+      (request.payload.line_2 ? request.payload.line_2.toLowerCase() : ''),
       request.payload.city.toLowerCase(),
       request.payload.postal_code.toLowerCase(),
       request.payload.state.toLowerCase(),
@@ -75,20 +76,45 @@ module.exports = function(request, reply) {
     connection.query(query, [
       request.payload.first_name,
       request.payload.last_name,
-      request.payload.organization,
+      request.payload.organization || null,
       request.payload.phone_number,
-      request.payload.email,
+      request.payload.email || null,
       addressId,
       now,
       request.params.contact_id
     ])
     .then(function(result) {
-      next(null, result.insertId);
+      next();
     });
-
   }
 
-  const sendReply = function(err, contactId) {
+  const updateContactInElasticsearch = function(next) {
+    elasticsearch.index({
+      index: 'address_book',
+      type: 'contact',
+      id: request.params.contact_id,
+      body: {
+        first_name: request.payload.first_name,
+        last_name: request.payload.last_name,
+        organization: request.payload.organization || null,
+        phone_number: request.payload.phone_number,
+        email: request.payload.email || null,
+        address: {
+          line_1: request.payload.line_1,
+          line_2: request.payload.line_2,
+          city: request.payload.city,
+          postal_code: request.payload.postal_code,
+          state: request.payload.state,
+          country: request.payload.country
+        }
+      }
+    })
+    .then(function(body) {
+      next();
+    });
+  };
+
+  const sendReply = function(err) {
     reply()
     .code(204);
   };
@@ -96,7 +122,8 @@ module.exports = function(request, reply) {
   async.waterfall([
     normalizeInput,
     insertOrSelectAddressFromDb,
-    updateContactInDb
+    updateContactInDb,
+    updateContactInElasticsearch
   ], sendReply);
 
 };
