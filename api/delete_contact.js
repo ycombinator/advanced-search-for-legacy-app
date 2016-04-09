cc24291c6f9fc101f2bae62753d115cca23af798
Shaunak Kashyap <ycombinator@gmail.com>
2016-04-09 15:13:38 -0500

    Augmenting delete contact code to delete from Elasticsearch

--- api/delete_contact.js
+++ api/delete_contact.js
@@ -1,15 +1,37 @@
+const async = require('async');
 const connection = require('./mysql_connection');
+const elasticsearch = require('./elasticsearch_connection');
 
 module.exports = function(request, reply) {
 
-  var query = "\
-    DELETE FROM contact \
-    WHERE id = ?";
+  const deleteContactFromDb = function(next) {
+    var query = "\
+      DELETE FROM contact \
+      WHERE id = ?";
 
-  connection.query(query, [ request.params.contact_id ])
-  .then(function(result) {
+    connection.query(query, [ request.params.contact_id ])
+    .then(function(result) {
+      next();
+    });
+  };
+
+  const deleteContactFromElasticsearch = function(next) {
+    elasticsearch.delete({
+      index: 'address_book',
+      type: 'contact',
+      id: request.params.contact_id
+    })
+    .then(next);
+  };
+
+  const sendReply = function() {
     reply()
     .code(204);
-  });
+  };
+
+  async.waterfall([
+    deleteContactFromDb,
+    deleteContactFromElasticsearch
+  ], sendReply);
 
 };
